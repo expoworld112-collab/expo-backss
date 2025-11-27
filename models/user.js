dacdec2b5bbb45e4d2ba1f7037f54e01f629d37c
expo <expoworld112@gmail.com>
2025-11-27 18:10:44 +0530

    dhf

--- models/user.js
+++ models/user.js
@@ -2,102 +2,92 @@ import mongoose from "mongoose";
 import crypto from "crypto";
 
 const userSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            trim: true,
-            required: true,
-            max: 32
-        },
-        username: {
-            type: String,
-            trim: true,
-            required: true,
-            max: 32,
-            lowercase: true,
-            unique:true
-        },
-        email: {
-            type: String,
-            trim: true,
-            required: true,
-            unique: true,
-            lowercase: true
-        },
-        profile: {
-            type: String,
-            required: true
-        },
-        hashed_password: {
-            type: String,
-            required: true
-        },
-        salt: String,
-        about: {
-            type: String
-        },
-        role: {
-            type: Number,
-            default: 0
-        },
-        photo: {
-            data: Buffer,
-            contentType: String
-        },
-        resetPasswordLink: {
-            data: String,
-            default: ''
-        }
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 32
     },
-    { 
-        timestamp: true
+    username: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 32,
+      lowercase: true,
+      unique: true
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+      lowercase: true
+    },
+    profile: {
+      type: String,
+      required: true
+    },
+    hashed_password: {
+      type: String,
+      required: true
+    },
+    salt: String,
+    about: {
+      type: String
+    },
+    role: {
+      type: Number,
+      default: 0
+    },
+    photo: {
+      data: Buffer,
+      contentType: String
+    },
+    resetPasswordLink: {
+      data: String,
+      default: ''
     }
+  },
+  { 
+    timestamps: true // fixed from 'timestamp: true'
+  }
 );
 
+// Virtual field for password
 userSchema
-    .virtual('password')
-    .set(function(password) {
-<<<<<<< HEAD
-        this._password = password;
-        this.salt = this.makeSalt();
-=======
-        // create a temporarity variable called _password
-        this._password = password;
-        // generate salt
-        this.salt = this.makeSalt();
-        // encryptPassword
->>>>>>> e8a0b69400c985ecdc3d87f5e13e4a922745963b
-        this.hashed_password = this.encryptPassword(password);
-    })
-    .get(function() {
-        return this._password;
-    });
+  .virtual('password')
+  .set(function(password) {
+    this._password = password;          // temporary variable
+    this.salt = this.makeSalt();        // generate salt
+    this.hashed_password = this.encryptPassword(password); // encrypt password
+  })
+  .get(function() {
+    return this._password;
+  });
 
+// Methods
 userSchema.methods = {
-    authenticate: function(plainText) {
-        return this.encryptPassword(plainText) === this.hashed_password;
-    },
+  authenticate: function(plainText) {
+    return this.encryptPassword(plainText) === this.hashed_password;
+  },
 
-    encryptPassword: function(password) {
-        if (!password) return '';
-        try {
-            return crypto
-                .createHmac('sha1', this.salt)
-                .update(password)
-                .digest('hex');
-        } catch (err) {
-            return '';
-        }
-    },
-
-    makeSalt: function() {
-        return Math.round(new Date().valueOf() * Math.random()) + '';
+  encryptPassword: function(password) {
+    if (!password) return '';
+    try {
+      return crypto
+        .createHmac('sha1', this.salt)
+        .update(password)
+        .digest('hex');
+    } catch (err) {
+      return '';
     }
+  },
+
+  makeSalt: function() {
+    return Math.round(new Date().valueOf() * Math.random()) + '';
+  }
 };
 
-<<<<<<< HEAD
-=======
-// module.exports = mongoose.model('User', userSchema);
->>>>>>> e8a0b69400c985ecdc3d87f5e13e4a922745963b
-export default mongoose.model('User', userSchema);
-
+const User = mongoose.models.User || mongoose.model('User', userSchema);
+export default User;
